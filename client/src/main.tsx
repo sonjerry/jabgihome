a583b6b70e439653ea33e033d9b2970068ea0e07
sonjerry <qh.e.720@icloud.com>
2025-08-12 20:31:10 +0900

    i18n 다지움

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './state/auth'
-import { I18nProvider } from './lib/i18n'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <I18nProvider>
+        
           <App />
-        </I18nProvider>
+        
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
